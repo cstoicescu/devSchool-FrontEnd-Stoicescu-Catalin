67b279f33ab2c36fd2b9ff4cbb7a418a1cd3d765
Catalin Stoicescu <stoicescu.catalinn97@gmail.com>
2020-04-02 20:09:49 +0300

    css, fetch js

---
+++ course-2/fetch.js
@@ -0,0 +1,35 @@
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//         .then(response => response.json())
+//         .then(json => console.log(json[0].userId, json[0].title));
+// fetch('https://jsonplaceholder.typicode.com/posts/2')
+//         .then(response => response.json())
+//         .then(json => console.log(json));
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        title: 'Test',
+        body: 'Test',
+        userId:2,
+    }),
+})
+       .then(response => response.json())
+       .then(json =>console.log(json));
+
+
+       
+       
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PATCH',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        title: 'Title Changed',
+    }),
+})
+       .then(response => response.json())
+       .then(json =>console.log(json));
+       
