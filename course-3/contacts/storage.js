47def106de25cc9bb69b8da3140a013995ee0d3e
Catalin Stoicescu <stoicescu.catalinn97@gmail.com>
2020-04-09 20:45:33 +0300

    contacts

---
+++ course-3/contacts/storage.js
@@ -0,0 +1,16 @@
+export function read() {
+    const data = window.localStorage.getItem('ds-contacts');   //in local storage tinem stringuri
+    return data === null ? [] : JSON.parse(data);
+}
+
+function write(contacts) {
+    const data = JSON.stringify(contacts);
+    window.localStorage.setItem('ds-contacts',data);
+}
+
+export function append(contact) {
+    const contacts = read();
+    contacts.push(contact);
+    write(contacts);
+
+}
---
+++ course-3/users/index.js
@@ -0,0 +1,44 @@
+const main = {
+    user : [],  // definire ---    cheie : valoare
+
+    error: "",
+
+    async init() {
+        console.log('hello', document.getElementById('list'));
+        const users = await this.fetchUsers();
+        if(users) {
+            this.users = users;
+            this.render();
+        }   else {
+             this.error = 'An error has occurred';
+        }
+        this.render();
+    },
+    async fetchUsers(){
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (response.ok)  {    // daca response are proprietate ok
+             const data = response.json();
+             return data;
+        }
+        return null;
+    },
+
+    render(){ 
+        if(this.error) {
+            document.getElementById('error').textContent = this.error;
+        }
+        else {
+            const items = this.users.map(users => `<li> ${users.name} ${users.email}</li>`);
+            const list = document.getElementById('list');
+            list.innerHTML = items.join('');
+        }
+    }
+
+};
+    
+     // apelare
+
+
+    // init:() => {};
+    // init: function() {};
+    // echivalente cu init()
