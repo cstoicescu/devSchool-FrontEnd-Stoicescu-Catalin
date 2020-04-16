53b0a011b8cd3694cfd41d34e3ed2da92c431d82
Catalin Stoicescu <stoicescu.catalinn97@gmail.com>
2020-04-16 18:38:37 +0300

    delete function added

--- course-3/contacts/index.js
+++ course-3/contacts/index.js
@@ -1,38 +1,59 @@
-import { append , read} from './storage.js';
+import { append, read, remove } from './storage.js';
 
 export function init() {
-    window.addEventListener('DOMContentLoaded',onLoad);
+  document.getElementById('form-add').addEventListener('submit', onSubmitAdd);
+  document.getElementById('form-delete').addEventListener('submit', onSubmitDelete);
+  document.getElementById('form-delete').addEventListener('change', onChangeDelete);
+  //   document.getElementById('form-delete').onsubmit = onSubmitDelete;   sau asa, fara apelul functiei,doar referinta
+  render();
 }
 
-function onLoad() {
-    document.getElementById('form-add').addEventListener('submit',onSubmitAdd)
-    document.getElementById('form-delete').addEventListener('submit',onSubmitDelete)
-    render();
+function onChangeDelete(event) {
+  const { form } = event.target;
+  // const form = event.target.form;
+  const data = new FormData(form); // sau event.currentTarget
+  const hasChecked = data.getAll('id').length > 0;
+  form.elements.delete.disabled = !hasChecked;
 }
 
 function onSubmitAdd(event) {
-    event.preventDefault();
-
-    const data = new FormData(event.target);
-    const contact = Object.fromEntries(data);
-    append(contact);
-    render();
+  event.preventDefault();
+  const form = event.target;
+  const data = new FormData(form);
+  data.set('id', Date.now());
+  const contact = Object.fromEntries(data);
+  append(contact);
+  render();
 }
 
 function onSubmitDelete(event) {
-    
+  event.preventDefault(); //altfel face refresh
+
+  const form = event.target; // se refera la listenerul de event, adica in cazul asta formul de delete
+  const data = new FormData(form);
+  const contacts = read();
+  data.getAll('id').forEach(id => {
+    const contact = contacts.find(contact => contact.id === id);
+    if (contact) {
+      remove(contact);
+    }
+  });
+  render();
 }
 
 function render() {
-    const contacts = read();
-    const list = document.getElementById('list');
-    const items = contacts.map(
-        contact => 
-        `<li>
-        <input type = "checkbox" name = "delete" /> 
-    ${contact.name} &lt;${contact.email}&gt; (${contact.phone})
-    </li>`);
-    list.innerHTML = items.join('');
-    const formDelete = document.getElementById('form-delete');
-    formDelete.hidden = contacts.length === 0;
+  const contacts = read();
+  const list = document.getElementById('list');
+  const items = contacts.map(
+    contact =>
+      `<li>
+        <label>
+        <input type = "checkbox" name = "id" value = "${contact.id}" /> 
+    ${contact.name} &lt;${contact.email}&gt ; (${contact.phone})
+        </label>
+    </li>`
+  );
+  list.innerHTML = items.join('');
+  const formDelete = document.getElementById('form-delete');
+  formDelete.hidden = contacts.length === 0;
 }
--- course-3/contacts/storage.js
+++ course-3/contacts/storage.js
@@ -1,16 +1,25 @@
 export function read() {
-    const data = window.localStorage.getItem('ds-contacts');   //in local storage tinem stringuri
-    return data === null ? [] : JSON.parse(data);
+  const data = window.localStorage.getItem('ds-contacts'); //in local storage tinem stringuri
+  return data === null ? [] : JSON.parse(data);
 }
 
 function write(contacts) {
-    const data = JSON.stringify(contacts);
-    window.localStorage.setItem('ds-contacts',data);
+  const data = JSON.stringify(contacts);
+  window.localStorage.setItem('ds-contacts', data);
+}
+
+export function remove(contact) {
+  const contacts = read();
+
+  const index = contacts.findIndex(element => element.id === contact.id);
+  if (index !== -1)
+    //verificam daca se gaseste, returneaza -1 daca nu a fost gasit.
+    contacts.splice(index, 1);
+  write(contacts);
 }
 
 export function append(contact) {
-    const contacts = read();
-    contacts.push(contact);
-    write(contacts);
-
+  const contacts = read();
+  contacts.push(contact);
+  write(contacts);
 }
