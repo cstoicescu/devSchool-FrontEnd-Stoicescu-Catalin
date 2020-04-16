23c2aec6175bfd0a86580b26924f71cbb015ec90
Catalin Stoicescu <stoicescu.catalinn97@gmail.com>
2020-04-16 10:19:33 +0300

    homework done final

--- homework-2/contacts/storage.js
+++ homework-2/contacts/storage.js
@@ -1,32 +1,35 @@
 export function read() {
-    const data = window.localStorage.getItem('ds-contacts');   //in local storage tinem stringuri
-    return data === null ? [] : JSON.parse(data);
+  const data = window.localStorage.getItem('ds-contacts'); //in local storage tinem stringuri
+  return data === null ? [] : JSON.parse(data);
 }
 
 function write(contacts) {
-    const data = JSON.stringify(contacts);
-    window.localStorage.setItem('ds-contacts',data);
+  const data = JSON.stringify(contacts);
+  window.localStorage.setItem('ds-contacts', data);
 }
 
 export function append(contact) {
-    const contacts = read();
-    contacts.push(contact);
-    write(contacts);
-
+  const contacts = read();
+  contact.id = contacts.length;
+  contacts.push(contact);
+  write(contacts);
 }
 
-export function deleteContacts (contactToRemove) {
-    let contacts = read();
-    let newContacts = contacts.filter(contact => (contact.name !== contactToRemove.name
-                || contact.email !== contactToRemove.email 
-               ||  contact.phone !== contactToRemove.phone))
+export function deleteContacts(contactToRemove) {
+  let contacts = read();
+  let newContacts = contacts.filter(
+    contact =>
+      contact.name !== contactToRemove.name ||
+      contact.email !== contactToRemove.email ||
+      contact.phone !== contactToRemove.phone ||
+      contact.id !== contactToRemove.id
+  );
 
-               
-    // let newContacts = contacts.filter(function(contact){
-    //     return contact.name !== contactToRemove.name
-    //         || contact.email !== contactToRemove.email 
-    //         ||  contact.phone !== contactToRemove.phone
-    // })
-    // console.log(contactToRemove.name);
-    write(newContacts);
+  // let newContacts = contacts.filter(function(contact){
+  //     return contact.name !== contactToRemove.name
+  //         || contact.email !== contactToRemove.email
+  //         ||  contact.phone !== contactToRemove.phone
+  // })
+  // console.log(contactToRemove.name);
+  write(newContacts);
 }
